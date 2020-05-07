566374b7c5f68981cf9cfd7ebc5a0b586a5d589d
Hazeque <64709171+Hazeque@users.noreply.github.com>
2020-05-07 22:45:58 +0300

    Add files via upload

---
+++ Practice 3.1 WF/Practice 3.1 WF/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practice_3._1_WF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a = Double.Parse(textBox1.Text);
+            double b = Double.Parse(textBox1.Text);
+            if (function(a) > function(b))
+                textBox3.Text = "Функция принимает наибольшее значение в точке а";
+            else
+                textBox3.Text = "Функция принимает наибольшее значение в точке b";
+        }
+        static double function(double x)
+        {
+            return x * x * x - Math.Sin(x);
+        }
+    }
+}
---
+++ Practice 3.1/Practice 3.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Practice_3._1
+{
+    class Program
+    {
+        static double function (double x)
+        {
+            return x * x * x - Math.Sin(x);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение а");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение b");
+            double b = Convert.ToDouble(Console.ReadLine());
+            double z = function(a);
+            if (function(a) > function(b))
+                Console.WriteLine("Функция принимает наибольшее значение в точке а");
+            else
+                Console.WriteLine("Функция принимает наибольшее значение в точке b");
+
+        }
+    }
+}
