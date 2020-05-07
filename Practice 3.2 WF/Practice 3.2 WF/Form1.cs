4fb0a6e1c137135e867fe42fa76d907a633c4a89
Hazeque <64709171+Hazeque@users.noreply.github.com>
2020-05-07 22:47:59 +0300

    Add files via upload

---
+++ Practice 3.2 WF/Practice 3.2 WF/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practice_3._2_WF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static double function(double x, double a)
+        {
+            if (x < a)
+                return 0;
+            if (x == a)
+                return 1;
+            else
+                return (x - a) / (x + a);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a = Double.Parse(textBox1.Text);
+            double b = Double.Parse(textBox2.Text);
+            double x = Double.Parse(textBox3.Text);
+            double h = Double.Parse(textBox4.Text);
+            if (x > a)
+            {
+                if (x + a == 0)
+                    MessageBox.Show("Знаменатель не может равняться 0", "Ошибка");
+                else
+                    while (x < b)
+                    {
+                        textBox5.Text += Convert.ToString(function(x, a)) + Environment.NewLine;
+                        x = x + h;
+                    }
+            }
+            else
+            {
+                while (x < b)
+                {
+                    textBox5.Text += Convert.ToString(function(x, a)) + Environment.NewLine;
+                    x = x + h;
+                }
+
+            }
+        }
+    }
+}
---
+++ Practice 3.2/Practice 3.2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Practice_3._2
+{
+    class Program
+    {
+        static double function(double x, double a)
+        {
+            if (x < a)
+                return 0;
+            if (x == a)
+                return 1;
+            else                  
+                return (x - a) / (x + a);         
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите a");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите b");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите шаг h");
+            double h = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите x");
+            double x = Convert.ToDouble(Console.ReadLine());
+            if (x > a)
+            {
+                if (x + a == 0)
+                    Console.WriteLine("Знаменатель не может равняться 0");
+                else
+                    while (x < b)
+                    {
+                        Console.WriteLine(function(x, a));
+                        x = x + h;
+                    }
+            }
+            else
+            {
+                while (x < b)
+                {
+                    Console.WriteLine(function(x, a));
+                    x = x + h;
+                }
+            }
+        }
+    }
+}
