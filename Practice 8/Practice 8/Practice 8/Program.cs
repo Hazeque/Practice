1a0b52cbb5d8c84c6bc69e7dd8a3ca63d803f054
Hazeque <64709171+Hazeque@users.noreply.github.com>
2020-10-29 01:12:15 +0300

    Add files via upload

---
+++ Practice 8/Practice 8/Practice 8/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Practice_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            Console.WriteLine("Введите строку");
+            string str = Console.ReadLine();
+            Regex regex = new Regex(@"\b[A-Z]\S*");
+            Console.WriteLine("Слова, начиющиеся с большой буквы: ");
+            foreach (var i in regex.Matches(str))
+                Console.WriteLine(i + " ");
+        }
+    }
+}
